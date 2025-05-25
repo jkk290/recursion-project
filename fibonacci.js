7a501e87cbb29e5101f84f3d6c45d9175bed1b39
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-05-24 17:48:42 -1000

    Added recursive function for fibonacci sequence

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,11 @@
 // Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the Fibonacci sequence. Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 
 function fibs(n) {
+     if (n == 0) {
+        return [];
+    } else if (n == 1) {
+        return [0];
+    }
     let fibArray = [];
     let num1 = 0;
     let num2 = 1;
@@ -18,20 +23,26 @@ function fibs(n) {
     return fibArray;
 };
 
-
-
-
-
-
 // Now write another function fibsRec which solves the same problem recursively.
 
-// function fibsRec(n, array) {
-//     if (n < 2 ) {
-//         return 1;
-//     }
+function fibsRec(n) {
+    if (n == 0) {
+        return [];
+    }
+    console.log("This was printed recursively");
 
+    if (n == 1) {
+        return [0];
+    } else if (n == 2) {
+        return [0, 1];
+    } else {
+        let prevSequence = fibsRec(n - 1);
+        let nextNum = prevSequence.at(-1) + prevSequence.at(-2);
+        prevSequence.push(nextNum);
 
+        return prevSequence;
+    }
 
-// }
+}
 
-console.log(fibs(8));
+console.log(fibsRec(20));
